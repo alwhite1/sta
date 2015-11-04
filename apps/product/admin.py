fe2bf7a14806993bcdef9a9051b0770ae3961c63
alwhite1 <alwhite1@rambler.ru>
2015-11-04 10:41:29 +0200

    Change register model in admin registration

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -2,12 +2,5 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-class ProductAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    exclude = ('slug',)
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Product)
